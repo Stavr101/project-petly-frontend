95f83836c1bf7365f87a2fb2164a42f4f483a4c6
Stavr101 <Shishkin_SU@ukr.net>
2023-01-24 17:46:13 +0200

    init 1.2

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import { BrowserRouter } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react';
-import { Provider } from 'react-redux';
-import { store, persistor } from './redux/store';
-import 'modern-normalize';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "components/App";
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import "modern-normalize";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="goit-react-hw-08-phonebook">
+        <BrowserRouter basename="project-petly-frontend">
           <App />
         </BrowserRouter>
       </PersistGate>

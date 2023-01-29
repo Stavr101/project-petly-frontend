789d9cf39e6a56762464f781d2564ad65228ca88
Sergey Ostroverkh <ostroverkh27@gmail.com>
2023-01-29 22:49:31 +0200

    update

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -22,7 +22,7 @@ export const register = createAsyncThunk(
   "auth/register",
   async (credentials, thunkAPI) => {
     try {
-      const res = await axios.post("register", credentials);
+      const res = await axios.post("/register", credentials);
       // After successful registration, add the token to the HTTP header
       setAuthHeader(res.data.token);
       return res.data;

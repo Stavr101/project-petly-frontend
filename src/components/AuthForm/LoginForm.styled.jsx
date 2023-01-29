8c7cb31ac800235c062b27fe92be1cd2c1be99d3
Sergey Ostroverkh <ostroverkh27@gmail.com>
2023-01-29 15:19:10 +0200

    update-login-page

--- src/components/AuthForm/LoginForm.styled.jsx
+++ src/components/AuthForm/LoginForm.styled.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { Form, Field } from "formik";
+import { Link } from "react-router-dom";
 
 export const FormTitle = styled.h1`
   font-family: inherit;
@@ -8,7 +9,7 @@ export const FormTitle = styled.h1`
   font-size: 24px;
   line-height: 33px;
   letter-spacing: 0.04em;
-  margin-top: 42px;
+  /* margin-top: 42px; */
   @media screen and (min-width: 768px) {
     font-size: 36px;
     line-height: 49px;
@@ -17,7 +18,7 @@ export const FormTitle = styled.h1`
 `;
 
 export const FormWrapper = styled.div`
-  padding: 20px;
+  padding: 0 20px;
 
   @media screen and (min-width: 768px) {
     width: 608px;
@@ -26,7 +27,8 @@ export const FormWrapper = styled.div`
     padding: 60px 80px 40px 80px;
     box-shadow: 7px 4px 14px rgba(0, 0, 0, 0.11);
     border-radius: 40px;
-    margin-top: 170px;
+    background-color: ${(p) => p.theme.colors.white};
+    /* margin-top: 170px; */
   }
   @media screen and (min-width: 1280px) {
     padding: 60px 80px;
@@ -72,4 +74,9 @@ export const Text = styled.p`
   line-height: 16px;
   letter-spacing: 0.04em;
   color: ${(p) => p.theme.colors.text.gray};
+`;
+
+export const LinkAuth = styled(Link)`
+  text-decoration: underline;
+  color: #3091eb;
 `;

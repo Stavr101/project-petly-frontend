921174a5f7b97c31c7e77d4083eca6afca703988
Dmitry Natalchuk <next1996@ukr.net>
2023-01-31 16:41:05 +0200

    restart task

--- src/components/NoticeCategoryItem/NoticeCategoryItem.jsx
+++ src/components/NoticeCategoryItem/NoticeCategoryItem.jsx
@@ -1,23 +1,7 @@
 import React from 'react'
 
-export default function NoticeCategoryItem({avatar,favorite,title,breed,location,age,condition,onDeletePets}) {
+export default function NoticeCategoryItem() {
   return (
-    <li>
-      <div>
-        <img src={avatar} alt="Pets" width="100" />
-        <p>{condition}</p>
-        <span>icon</span>
-      </div>
-    <h3>{title}</h3>
-      <p>Breed:{breed}</p> 
-      <p>Location:{location}</p>
-      <p>Age:{age}</p> 
-    <button>Learn more</button>
-    {favorite? <button
-      type='submit'
-      onClick={() => onDeletePets(id)}>
-      Delete
-    </button> : null}
-    </li>
+    <div>NoticeCategoryItem</div>
   )
 }
--- src/components/NoticesCategoriesList/NoticesCategoriesList.jsx
+++ src/components/NoticesCategoriesList/NoticesCategoriesList.jsx
@@ -1,23 +1,6 @@
-import React from 'react'
 
-import NoticeCategoryItem from 'components/NoticeCategoryItem/NoticeCategoryItem'
-
-export default function NoticesCategoriesList(userPets) {
-  const filterPets = userPets.filter(pets => pets.condition === condition)
+export default function NoticesCategoriesList() {
   return (
-    <ul key={filterPets.id}>
-      {filterPets.map(({id,avatar,favorite,title,breed,location,age,condition}) => (
-        <NoticeCategoryItem
-        key={id}
-        avatar = {avatar}
-        favorite = {favorite}
-        title = {title}
-        breed = {breed}
-        location = {location}
-        age = {age}
-        condition = {condition}
-        />
-      ))}
-    </ul>  
+    <div>NoticesCategoriesList</div>
   )
 }

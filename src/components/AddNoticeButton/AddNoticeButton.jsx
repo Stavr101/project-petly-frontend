f03e9d9fd4b8f81bda9be5fb9e7c3870690371bc
Myroslav Semeniuk <97482927+myrsem@users.noreply.github.com>
2023-02-04 19:42:20 +0200

    fix notices

--- src/components/AddNoticeButton/AddNoticeButton.jsx
+++ src/components/AddNoticeButton/AddNoticeButton.jsx
@@ -8,19 +8,22 @@ import {
 } from "components/AddNoticeButton/AddNoticeButton.styled";
 import { Notify } from "notiflix/build/notiflix-notify-aio";
 import { useAuth } from 'hooks';
-import ModalAddNoticeApp from "components/ModalAddNotice/ModalAddNoticelApp"
+import ModalAddsPetApp from 'components/ModalAddsPet/ModalAddsPetsApp';
 
 export default function AddNoticeButton() {
-  const [showModal, setShowModal] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   const { isLoggedIn } = useAuth();
 
   const handleClick = () => {
+    
     if (!isLoggedIn) {
     Notify.failure("Please log in", {
       position: 'center-top',
       cssAnimationStyle: 'from-right',
     })}
-    setShowModal(true);
+    else {
+    setIsOpen(true);
+  }
   }
 
   return (
@@ -31,12 +34,13 @@ export default function AddNoticeButton() {
           <Icon />
           <Cont>Add pet</Cont>
         </Button>
-      </Wrap>
-      {showModal && (
-        <ModalAddNoticeApp
-          setShowModal={setShowModal}
+        {isOpen ? (
+        <ModalAddsPetApp
+          onOpenModal={isOpen}
+          onCloseModal={() => setIsOpen(false)}
         />
-      )}
+      ) : null}
+      </Wrap>
     </>
   );
 }

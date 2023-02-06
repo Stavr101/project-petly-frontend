cb3b3c240e41e1ec8060a72a97c5330c0ffa9567
nadtosonna <prasolova.kateryna@gmail.com>
2023-02-06 22:21:45 +0200

    home page small fix

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,16 @@
 import MainPageContainer from "components/Container/MainPageContainer/MainPageContainer";
-import { MainHeader } from "./Home.styled";
-import { MainImageBlock } from "./Home.styled";
-import { SectionHero } from "./Home.styled";
+import { MainPage, MainHeader, SectionHero, MainImageBlock } from "./Home.styled";
 
 const Home = () => {
   return (
-    <main>
+    <MainPage>
       <SectionHero>
         <MainPageContainer>
           <MainHeader>Take good care of your small pets</MainHeader>
         </MainPageContainer>
         <MainImageBlock></MainImageBlock>
       </SectionHero>
-    </main>
+    </MainPage>
   );
 }
 

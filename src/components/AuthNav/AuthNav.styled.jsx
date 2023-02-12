516f5580bf9fa838905951a0ed3fca2577315739
nadtosonna <prasolova.kateryna@gmail.com>
2023-02-12 22:50:11 +0200

    hovers fixes

--- src/components/AuthNav/AuthNav.styled.jsx
+++ src/components/AuthNav/AuthNav.styled.jsx
@@ -18,7 +18,7 @@ letter-spacing: 0.04em;
 font-family: ${p => p.theme.fonts.manrope};
 font-size: 14px;
 line-height: 1.35;
-transition: color 250ms cubic-bezier(0.4, 0, 0.2, 1), font-weight 250ms cubic-bezier(0.4, 0, 0.2, 1), box-shadow 250ms;
+transition: color 250ms cubic-bezier(0.4, 0, 0.2, 1), background-color 250ms cubic-bezier(0.4, 0, 0.2, 1), box-shadow 250ms;
 
 @media (min-width: 768px) {
 padding: 8.5px 28px;
@@ -30,15 +30,13 @@ padding: 10px 28px;
 }
 
 &:hover {
-    color: ${p => p.theme.colors.accent};
-    font-weight: ${(p) => p.theme.fontWeights.bold};
+    background-color: ${p => p.theme.colors.accent};
+    color: ${p => p.theme.colors.white};
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 }
 
 &.active {
     background-color: ${p => p.theme.colors.accent};
     color: ${p => p.theme.colors.white};
-    transition: color 250ms cubic-bezier(0.4, 0, 0.2, 1);
-    transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
 }
 `

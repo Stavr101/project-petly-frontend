cfc62390aedaa5187305cc3544aeacf659b23a59
Alona Beskhlibnova <a.beskhlibnova@gmail.com>
2023-02-13 03:43:50 +0200

    fix pets cards

--- src/pages/User/User.js
+++ src/pages/User/User.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import UserData from 'components/UserData/UserData';
 import PetsData from 'components/PetsData/PetsData';
 import { UserPageWrapper } from './User.styled';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { getUserData } from 'redux/users/selectors';
 import { getPets } from 'redux/pets/selectors';
+import { getPetInfo } from 'redux/pets/operations';
 
 export default function User() {
   const user = useSelector(getUserData);
   const pets = useSelector(getPets);
+
   return (
     <UserPageWrapper>
       <UserData user={user} />
